f295d8817c9b2edd4e11fc5143b5a3b2a7d30fbe
Nihaal <nayaknihaal04@gmail.com>
2026-07-20 19:01:27 +0530

    initialized city management api

--- microservices/services/location-service/src/main/java/com/project/location_service/repository/CityRepository.java
+++ microservices/services/location-service/src/main/java/com/project/location_service/repository/CityRepository.java
@@ -1,6 +1,6 @@
 package com.project.location_service.repository;
 
-import org.springframework.boot.data.autoconfigure.web.DataWebProperties.Pageable;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
